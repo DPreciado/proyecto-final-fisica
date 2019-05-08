72ab07c4cc999ad1afe34bb4c4e5fa5a94fe332b
David <47166040+FootlessCat985@users.noreply.github.com>
2019-05-07 23:11:25 -0700

    Ojos
    Se le agrego textura y color a los ojos, se puede cambiar el color en cualquier momento

---
+++ Assets/Scripts/HornetEyesColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HornetEyesColor : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    float colorPower = 1f;
+
+    [SerializeField]
+    Slider sld_colorPower;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        render.material.SetFloat("_Slider_Color", sld_colorPower.value);
+    }
+}
